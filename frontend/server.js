15f1b36cef1dd7e767941c4a5ea23bf388af0f72
Piyush Khurana <lucifer@kali.from-delhi.net>
2025-11-29 22:42:30 +0530

    change frontend port to 3000

--- frontend/server.js
+++ frontend/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
