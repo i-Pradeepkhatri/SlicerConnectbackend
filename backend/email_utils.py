c1b76293f05aa86b35a7ec611cebd78396305e54
i-Pradeepkhatri <pk96509354@gmail.com>
2026-02-05 20:43:41 +0530

    Fix email env defaults and ignore env files

--- backend/email_utils.py
+++ backend/email_utils.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 import os
 
 SMTP_SERVER = os.getenv("MAIL_SERVER")
-SMTP_PORT = int(os.getenv("MAIL_PORT"))
+SMTP_PORT = int(os.getenv("MAIL_PORT", 587))
 EMAIL_USER = os.getenv("MAIL_USER")
 EMAIL_PASS = os.getenv("MAIL_PASS")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
